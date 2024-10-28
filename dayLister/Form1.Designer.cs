7c21a009ad9036d2602814e42004d43428a3020c
nsm01l <nsm01l@hotmail.com>
2024-10-28 22:00:15 +0900

    プロジェクトの追加

---
+++ dayLister/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace dayLister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateStart = new System.Windows.Forms.DateTimePicker();
+            this.dateEnd = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.weekStart = new System.Windows.Forms.ComboBox();
+            this.weekEnd = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.txtGen = new System.Windows.Forms.TextBox();
+            this.chkIsLonger = new System.Windows.Forms.CheckBox();
+            this.txtFormatRange = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFormatOneday = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dateStart
+            // 
+            this.dateStart.Location = new System.Drawing.Point(12, 34);
+            this.dateStart.Name = "dateStart";
+            this.dateStart.Size = new System.Drawing.Size(117, 19);
+            this.dateStart.TabIndex = 0;
+            // 
+            // dateEnd
+            // 
+            this.dateEnd.Location = new System.Drawing.Point(163, 34);
+            this.dateEnd.Name = "dateEnd";
+            this.dateEnd.Size = new System.Drawing.Size(117, 19);
+            this.dateEnd.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "開始日:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "終了日:";
+            // 
+            // weekStart
+            // 
+            this.weekStart.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.weekStart.FormattingEnabled = true;
+            this.weekStart.Items.AddRange(new object[] {
+            "日",
+            "月",
+            "火",
+            "水",
+            "木",
+            "金",
+            "土"});
+            this.weekStart.Location = new System.Drawing.Point(12, 92);
+            this.weekStart.Name = "weekStart";
+            this.weekStart.Size = new System.Drawing.Size(117, 20);
+            this.weekStart.TabIndex = 4;
+            // 
+            // weekEnd
+            // 
+            this.weekEnd.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.weekEnd.FormattingEnabled = true;
+            this.weekEnd.Items.AddRange(new object[] {
+            "日",
+            "月",
+            "火",
+            "水",
+            "木",
+            "金",
+            "土"});
+            this.weekEnd.Location = new System.Drawing.Point(163, 92);
+            this.weekEnd.Name = "weekEnd";
+            this.weekEnd.Size = new System.Drawing.Size(117, 20);
+            this.weekEnd.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "曜日(終了):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "曜日(開始):";
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(190, 132);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(75, 23);
+            this.btnGen.TabIndex = 8;
+            this.btnGen.Text = "生成";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // txtGen
+            // 
+            this.txtGen.Location = new System.Drawing.Point(12, 161);
+            this.txtGen.Multiline = true;
+            this.txtGen.Name = "txtGen";
+            this.txtGen.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtGen.Size = new System.Drawing.Size(268, 110);
+            this.txtGen.TabIndex = 9;
+            this.txtGen.DoubleClick += new System.EventHandler(this.txtGen_DoubleClick);
+            // 
+            // chkIsLonger
+            // 
+            this.chkIsLonger.AutoSize = true;
+            this.chkIsLonger.Location = new System.Drawing.Point(14, 136);
+            this.chkIsLonger.Name = "chkIsLonger";
+            this.chkIsLonger.Size = new System.Drawing.Size(170, 16);
+            this.chkIsLonger.TabIndex = 10;
+            this.chkIsLonger.Text = "開始・終了は日にちを重視する";
+            this.chkIsLonger.UseVisualStyleBackColor = true;
+            // 
+            // txtFormatRange
+            // 
+            this.txtFormatRange.Location = new System.Drawing.Point(49, 282);
+            this.txtFormatRange.Name = "txtFormatRange";
+            this.txtFormatRange.Size = new System.Drawing.Size(231, 19);
+            this.txtFormatRange.TabIndex = 11;
+            this.txtFormatRange.Text = "%md1% - %md2%";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "範囲:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 310);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "1日:";
+            // 
+            // txtFormatOneday
+            // 
+            this.txtFormatOneday.Location = new System.Drawing.Point(49, 307);
+            this.txtFormatOneday.Name = "txtFormatOneday";
+            this.txtFormatOneday.Size = new System.Drawing.Size(231, 19);
+            this.txtFormatOneday.TabIndex = 13;
+            this.txtFormatOneday.Text = "%md%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 336);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtFormatOneday);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtFormatRange);
+            this.Controls.Add(this.chkIsLonger);
+            this.Controls.Add(this.txtGen);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.weekEnd);
+            this.Controls.Add(this.weekStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateEnd);
+            this.Controls.Add(this.dateStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DayLister";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateStart;
+        private System.Windows.Forms.DateTimePicker dateEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox weekStart;
+        private System.Windows.Forms.ComboBox weekEnd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.TextBox txtGen;
+        private System.Windows.Forms.CheckBox chkIsLonger;
+        private System.Windows.Forms.TextBox txtFormatRange;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFormatOneday;
+    }
+}
+
---
+++ dayLister/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace dayLister
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 初期値指定
+            weekStart.SelectedItem = "月";
+            weekEnd.SelectedItem = "金";
+        }
+
+        /// <summary>
+        /// 生成ボタンを押したら日付一覧を生成する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            // 開始日時と終了日時の取得
+            DateTime st = dateStart.Value;
+            DateTime ed = dateEnd.Value;
+
+            // 開始曜日と終了曜日の数値化
+            int weekStartIndex = weekStart.SelectedIndex;
+            int weekEndIndex = weekEnd.SelectedIndex;
+
+            if (weekStartIndex == weekEndIndex)
+            {
+                txtGen.Text = "開始週と終了週が同じです";
+                return;
+            }
+
+            // 曜日の範囲を数値化
+            // 表示する範囲の幅
+            int rangeWeek = (weekStartIndex < weekEndIndex) ? weekEndIndex - weekStartIndex : (7 - weekEndIndex) + weekStartIndex;
+            // 次に行くためのステップ
+            int nextStepWeek = (weekEndIndex > weekStartIndex) ? (7 - weekEndIndex) + weekStartIndex : weekStartIndex - weekEndIndex;
+
+            // 開始日時の曜日を揃える
+            DateTime counter;
+            int stWeek = Convert.ToInt32(st.DayOfWeek); // 開始日の曜日
+
+            // 生成文字列
+            string txt = "";
+            
+            string fmt2Day = txtFormatRange.Text;
+            string fmt1Day = txtFormatOneday.Text;
+
+            // 開始日優先ならば、開始曜日にかかわらず表示する
+            if (chkIsLonger.Checked)
+            {
+                DateTime tmp;
+                if (weekEndIndex > stWeek)
+                {
+                    tmp = st.AddDays(weekEndIndex - stWeek);
+                    txt += formatString(fmt2Day, st, tmp);
+                }
+                else if (stWeek > weekEndIndex)
+                {
+                    tmp = st.AddDays(7 - weekEndIndex + stWeek);
+                    txt += formatString(fmt2Day, st, tmp);
+                }
+                else
+                {
+                    tmp = new DateTime(st.Year, st.Month, st.Day);
+                    txt += formatString(fmt1Day, st);
+                }
+            }
+
+            // ループの開始日を設定
+            if (weekStartIndex > stWeek)
+            {
+                counter = st.AddDays(weekStartIndex - stWeek);
+            }
+            else if (weekStartIndex < stWeek)
+            {
+                counter = st.AddDays(7 - stWeek + weekStartIndex);
+            }
+            else
+            {
+                counter = new DateTime(st.Year, st.Month, st.Day);
+            }
+            
+            // 一覧の生成
+            DateTime next = counter.AddDays(+rangeWeek);    // 末尾
+            while (next < ed || isSameDate(next, ed))
+            {
+                txt += formatString(fmt2Day, counter, next);
+                // 開始日時の曜日を揃える
+                counter = next.AddDays(+nextStepWeek);
+                next = counter.AddDays(+rangeWeek);
+            }
+
+            // 終了日優先ならば、終了曜日にかかわらず表示する
+            if (chkIsLonger.Checked)
+            {
+                if (counter < ed)
+                {
+                    txt += formatString(fmt2Day, counter, ed);
+                }
+                else if (isSameDate(counter, ed))
+                {
+                    txt += formatString(fmt1Day, counter);
+                }
+            }
+            txtGen.Text = txt.TrimEnd();
+        }
+
+        // txtGen がダブルクリックされたら全選択する
+        private void txtGen_DoubleClick(object sender, EventArgs e)
+        {
+            txtGen.SelectAll();
+        }
+
+        // 2 つの年月日が等しいか調べる
+        static private Boolean isSameDate(DateTime a, DateTime b)
+        {
+            return (a.Year == b.Year && a.Month == b.Month && a.Day == b.Day);
+        }
+
+        /// <summary>
+        /// 書式に応じて文字列を出力する
+        /// </summary>
+        /// <param name="fmt"></param>
+        /// <param name="d1"></param>
+        /// <param name="d2"></param>
+        /// <returns></returns>
+        static private string formatString(string fmt, DateTime d1, DateTime d2)
+        {
+            // %md*%
+            fmt = fmt.Replace("%md1%", d1.Month + "/" + d1.Day);
+            fmt = fmt.Replace("%md2%", d2.Month + "/" + d2.Day);
+            // %ymd+%
+            fmt = fmt.Replace("%ymd1%", d1.Year + "/" + d1.Month + "/" + d1.Day);
+            fmt = fmt.Replace("%ymd2%", d2.Year + "/" + d2.Month + "/" + d2.Day);
+            // %y*%
+            fmt = fmt.Replace("%y1%", d1.Year.ToString());
+            fmt = fmt.Replace("%y2%", d2.Year.ToString());
+            // %m*%
+            fmt = fmt.Replace("%m1%", d1.Month.ToString());
+            fmt = fmt.Replace("%m2%", d2.Month.ToString());
+            // %d*%
+            fmt = fmt.Replace("%d1%", d1.Day.ToString());
+            fmt = fmt.Replace("%d2%", d2.Day.ToString());
+
+            return fmt + "\r\n";
+        }
+
+        /// <summary>
+        /// 書式の定義
+        /// </summary>
+        /// <param name="fmt"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        static private string formatString(string fmt, DateTime d)
+        {
+            // %md%
+            fmt = fmt.Replace("%md%", d.Month + "/" + d.Day);
+            // %ymd%
+            fmt = fmt.Replace("%ymd%", d.Year + "/" + d.Month + "/" + d.Day);
+            // %y%
+            fmt = fmt.Replace("%y%", d.Year.ToString());
+            // %m%
+            fmt = fmt.Replace("%m%", d.Month.ToString());
+            // %d%
+            fmt = fmt.Replace("%d%", d.Day.ToString());
+
+            return fmt + "\r\n";
+        }
+    }
+}
